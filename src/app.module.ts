256c306f4ab784b9246d529b0062d89765b24839
1-aminadab <developer.amanuel@gmail.com>
2024-05-17 00:40:25 +0300

    abstract away the Prisma Client API from the application

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 
+
 @Module({
   imports: [PrismaModule],
   controllers: [AppController],
