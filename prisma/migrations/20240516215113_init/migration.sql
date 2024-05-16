245a8031960e638f2ff902475265e0ea37a5b3cf
1-aminadab <developer.amanuel@gmail.com>
2024-05-17 01:02:42 +0300

    swagger documentaion added

---
+++ prisma/migrations/20240516215113_init/migration.sql
@@ -0,0 +1,27 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Customer` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Invoice` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `InvoiceItem` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
