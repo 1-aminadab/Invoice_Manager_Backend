be1d721e3b443efe48df954861edfda2ca5b3d4a
1-aminadab <developer.amanuel@gmail.com>
2024-05-19 16:20:57 +0300

    jwt and passport installed and acces token and refresh token strategy implemented

--- src/auth/strategies/access-token.strategy.ts
+++ src/auth/strategies/access-token.strategy.ts
@@ -1,6 +1,8 @@
+import { Injectable } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
 import { ExtractJwt, Strategy } from "passport-jwt";
 
+@Injectable()
 export class AccessTokenStrategy extends PassportStrategy(Strategy, 'jwt'){
     constructor() {
         super({
--- src/auth/strategies/refresh-token.strategy.ts
+++ src/auth/strategies/refresh-token.strategy.ts
@@ -1,14 +1,18 @@
+import { Injectable } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
+import { Request } from "express";
 import { ExtractJwt, Strategy } from "passport-jwt";
 
+@Injectable()
 export class RefreshTokenStrategy extends PassportStrategy(Strategy, 'jwt'){
     constructor() {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-            secretOfKey: 'at-secret'
+            secretOfKey: 'rt-secret',
+            passReqToCallback:true
         })
     }
-    validate(payload:any){
+    validate(req:Request,payload:any){
         return payload 
         
      }
